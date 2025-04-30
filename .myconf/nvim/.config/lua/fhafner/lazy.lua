5d843c618ac7c59f867c7b09a1ca4b5ca79dd843
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-04-30 10:35:28 +0200

    monthly snapshot

--- .myconf/nvim/.config/lua/fhafner/lazy.lua
+++ .myconf/nvim/.config/lua/fhafner/lazy.lua
@@ -42,7 +42,15 @@ require("lazy").setup({
     {'hrsh7th/nvim-cmp'},
     {"williamboman/mason.nvim"},
     {"williamboman/mason-lspconfig.nvim"},
-
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 
   },
   -- Configure any other settings here. See the documentation for more details.
