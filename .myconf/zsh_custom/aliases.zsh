b7d219f3445a1dab8ae3ab06c9f2d783bc86b111
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-04-30 10:35:17 +0200

    add alias

--- .myconf/zsh_custom/aliases.zsh
+++ .myconf/zsh_custom/aliases.zsh
@@ -7,11 +7,5 @@ PRJS="/home/flavio/repositories/projects/"
 alias prj_reditsap='cdactivate $PRJS/amuse/amuse conda Amuse-env'
 alias prj_l2v='cdactivate $PRJS/odissei-life2vec/life-sequencing-dutch venv'
 alias prj_dms_cpl='cdactivate $PRJS/DEMOSES/demoses-coupling venv'
-
-
-
-
-
-
-
+alias prj_dms_scl='cdactivate $PRJS/DEMOSES/demoses-scaling-benchmarks venv'
 
