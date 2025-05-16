07b44400dfbad0123a0b89b63782e319d4024f14
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-05-16 15:56:15 +0200

    update aliases

--- .myconf/zsh_custom/aliases.zsh
+++ .myconf/zsh_custom/aliases.zsh
@@ -3,9 +3,11 @@ alias workon=workon_venv
 
 alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
 
-PRJS="/home/flavio/repositories/projects/"
+PRJS="$HOME/repositories/projects/"
 alias prj_reditsap='cdactivate $PRJS/amuse/amuse conda Amuse-env'
 alias prj_l2v='cdactivate $PRJS/odissei-life2vec/life-sequencing-dutch venv'
 alias prj_dms_cpl='cdactivate $PRJS/DEMOSES/demoses-coupling venv'
 alias prj_dms_scl='cdactivate $PRJS/DEMOSES/demoses-scaling-benchmarks venv'
 
+alias kitty_dark="kitty --detach"
+alias kitty_light="kitty --config $HOME/.myconf/kitty/kitty_light.conf --detach"
