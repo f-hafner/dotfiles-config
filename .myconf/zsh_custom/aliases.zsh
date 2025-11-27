f89098ab1ea2c3b8fd9e2008cbd0c275c138ebc7
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-11-27 11:40:27 +0100

    Update popylar path

--- .myconf/zsh_custom/aliases.zsh
+++ .myconf/zsh_custom/aliases.zsh
@@ -8,7 +8,7 @@ alias prj_reditsap='cdactivate $PRJS/amuse/amuse conda Amuse-env'
 alias prj_l2v='cdactivate $PRJS/odissei-life2vec/life-sequencing-dutch venv'
 alias prj_annular='cdactivate $PRJS/DEMOSES/annular venv'
 alias prj_dms_scl='cdactivate $PRJS/DEMOSES/demoses-scaling-benchmarks venv'
-alias prj_pplr='cdactivate $PRJS/popylar/popylar-prf/ venv'
+alias prj_pplr='cdactivate $PRJS/popylar/prfmodel/ venv'
 
 alias kitty_dark="kitty --detach"
 alias kitty_light="kitty --config $HOME/.myconf/kitty/kitty_light.conf --detach"
